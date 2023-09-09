8d7c29fd2deadfc898ceebf8624108367332d79d
Monica <moha0001@stud.kea.dk>
2023-09-09 15:28:31 +0200

    product js coding under construction

--- javascript/productlist.js
+++ javascript/productlist.js
@@ -2,7 +2,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const strand = urlParams.get("strand");
 const categoryParam = strand ? "?strand=" + strand : "";
 
-// supabase link
+//https://uutiizppilsesjcptglo.supabase.co
 const key =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1dGlpenBwaWxzZXNqY3B0Z2xvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNzQ0MTgsImV4cCI6MjAwOTY1MDQxOH0.NUGKP51o37ORWFiRq3CrUp7oImAREwLfwuA5LWPaK2I";
 
