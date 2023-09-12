d2e810fb2588cfea84fab759aa45cee574ab7696
Nina <nihi0001@kea.dk>
2023-09-12 20:47:03 +0200

    forsøg på burgermenu

--- javascript/script.js
+++ javascript/script.js
@@ -24,8 +24,32 @@ const winter = document.querySelector("#winter-button").textContent;
 // const summer = document.querySelector("#summer-button").content;
 // const fall = document.querySelector("#fall-button").content;
 
-function showWinterCategory(winterCategory) {
-  if ((database.season / 0, 1, 2, 3 === "11, 0, 1"));
+// burger menu
+// Lav variablen "btn", der henviser til ".toggle-btn"
+const btn = document.querySelector(".toggle-btn");
+// Lav variablen "menu", der henviser til ".main-menu"
+const menu = document.querySelector(".main-menu");
 
-  console.log(winterCategory);
+// Lav funktionen "toggleMenu()" med følgende funktionalitet
+function toggleMenu() {
+  // a) toggle klassen "shown" på menu vha. classList.toggle
+  menu.classList.toggle("shown");
+
+  // b) lav variablen "menuShown", som siger, at menu-variablen indeholder klassen "shown" via classList.contains("")
+  const menuShown = menu.classList.contains("shown");
+
+  // c) spørg om "menuShown" i if-sætningen nedenfor (=> if (menuShown)), og udskift teksten
+  if (menuShown) {
+    console.log(menuShown); // se i konsollen
+    // sæt btn.textContent til "Luk", hvis menuShown er "true"
+    btn.textContent = "Luk";
+  } else {
+    console.log(menuShown); // se i konsollen
+    // sæt btn.textContent til "Menu", hvis menuShown er "false"
+    btn.textContent = "Menu";
+  }
 }
+
+// Tilføj et klik-event til "btn", der sætter toggleMenu-funktionen i gang
+btn.addEventListener("click", toggleMenu);
+
