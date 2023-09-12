f57ccfd61cb72d357f3c7da2e65fe0a30e04405c
Monica <moha0001@stud.kea.dk>
2023-09-12 14:34:54 +0200

    ny database virker på produktliste

--- javascript/product.js
+++ javascript/product.js
@@ -1,4 +1,4 @@
-//
+//https://uutiizppilsesjcptglo.supabase.co/rest/v1/vildmad_database
 
 const key =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1dGlpenBwaWxzZXNqY3B0Z2xvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNzQ0MTgsImV4cCI6MjAwOTY1MDQxOH0.NUGKP51o37ORWFiRq3CrUp7oImAREwLfwuA5LWPaK2I";
@@ -22,18 +22,19 @@ fetch(
 function showStrand(strand) {
   const data = strand[0];
   document.querySelector("p.id").textContent = data.id;
-  document.querySelector(".title").textContent = data.title;
-  document.querySelector("sankelandskaber/0/title").textContent =
-    data.sankelandskaber;
+  document.querySelector(".productBox h2").textContent = data.title;
+  document.querySelector(".productBox h3").textContent =
+    data.sankelandskaber_title;
+
+  document.querySelector(".imageprodukt").src = data.profile_image;
 
   //document.querySelector(".grid_product".appendChild(clone);
 }
 
-const menu_btn = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.mobile-nav')
+const menu_btn = document.querySelector(".hamburger");
+const mobile_menu = document.querySelector(".mobile-nav");
 
-menu_btn.addEventListener('click', function(){
-    menu_btn.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
-
+menu_btn.addEventListener("click", function () {
+  menu_btn.classList.toggle("is-active");
+  mobile_menu.classList.toggle("is-active");
 });
--- javascript/productlist.js
+++ javascript/productlist.js
@@ -6,7 +6,7 @@ const categoryParam = strand ? "?strand=" + strand : "";
 const key =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1dGlpenBwaWxzZXNqY3B0Z2xvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNzQ0MTgsImV4cCI6MjAwOTY1MDQxOH0.NUGKP51o37ORWFiRq3CrUp7oImAREwLfwuA5LWPaK2I";
 
-fetch("https://uutiizppilsesjcptglo.supabase.co/rest/v1/vildmad_database", {
+fetch("https://uutiizppilsesjcptglo.supabase.co/rest/v1/vildmadDatabase", {
   method: "GET",
   headers: {
     apikey: key,
@@ -25,7 +25,7 @@ function showStrand(strand) {
   const copy = template.cloneNode(true);
 
   copy.querySelector(".title").textContent = strand.title;
-  copy.querySelector("img").src = strand.profile_image;
+  copy.querySelector("img").src = strand.profileImage;
 
   copy
     .querySelector(".imageProductlist")
