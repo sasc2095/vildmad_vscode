8e8e861d41fa686fcbd33c77a405167ea0af2767
Monica <moha0001@stud.kea.dk>
2023-09-12 20:48:34 +0200

    kommentare til js

--- javascript/productlist.js
+++ javascript/productlist.js
@@ -1,20 +1,25 @@
+//opretter konstanter, der lytter til windows load samt laver urlparameter for hvilken data der skal hentes ned.
 const urlParams = new URLSearchParams(window.location.search);
 const strand = urlParams.get("strand");
 const categoryParam = strand ? "?strand=" + strand : "";
 
+//sætter en variable for vores nøgle og udkommentere, hvilken side vi har den fra.
 //https://uutiizppilsesjcptglo.supabase.co
 const key =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1dGlpenBwaWxzZXNqY3B0Z2xvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNzQ0MTgsImV4cCI6MjAwOTY1MDQxOH0.NUGKP51o37ORWFiRq3CrUp7oImAREwLfwuA5LWPaK2I";
 
+//fetcher data fra database
 fetch("https://uutiizppilsesjcptglo.supabase.co/rest/v1/vildmadDatabase", {
   method: "GET",
   headers: {
     apikey: key,
   },
 })
+  //skriver i js, at vi gerne vil have dens response er json
   .then((res) => res.json())
   .then(showStrande);
 
+//opretter en function, hvor vi looper arrayet enkeltvis for hver nedenstående kloning
 function showStrande(strande) {
   strande.forEach(showStrand);
 }
@@ -35,11 +40,10 @@ function showStrand(strand) {
 
 // burgermenu
 
-const menu_btn = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.mobile-nav')
+const menu_btn = document.querySelector(".hamburger");
+const mobile_menu = document.querySelector(".mobile-nav");
 
-menu_btn.addEventListener('click', function(){
-    menu_btn.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
-
+menu_btn.addEventListener("click", function () {
+  menu_btn.classList.toggle("is-active");
+  mobile_menu.classList.toggle("is-active");
 });
