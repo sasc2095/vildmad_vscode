9c77d38e1048759875aabdd1f5caa975d40badbf
Sascha Nielsen <sasc2095@stud.kea.dk>
2023-09-13 12:26:01 +0200

    2

--- javascript/script.js
+++ javascript/script.js
@@ -10,38 +10,31 @@ const urlParams = new URLSearchParams(window.location.search);
 const strand = urlParams.get("strand");
 const categoryParam = strand ? "?strand=" + strand : "";
 
-fetch("https://uutiizppilsesjcptglo.supabase.co/rest/v1/vildmad_database", {
+fetch("https://uutiizppilsesjcptglo.supabase.co/rest/v1/vildmadDatabase", {
   method: "GET",
   headers: {
     apikey:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1dGlpenBwaWxzZXNqY3B0Z2xvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNzQ0MTgsImV4cCI6MjAwOTY1MDQxOH0.NUGKP51o37ORWFiRq3CrUp7oImAREwLfwuA5LWPaK2I",
   },
 })
-.then((response) => response.json())
-.then((data) => showProduct(data));
+  .then((response) => response.json())
+  .then((data) => showProduct(data));
 
-function showProducts(products){
+function showProducts(products) {
   products.forEach(showProduct);
 }
 
-  function showProduct(product) {
-    console.log(product);
-    const dataer = product[0];
+function showProduct(product) {
+  console.log(product);
+  const data = product[0];
 
   const template = document.querySelector(".winter-button").content;
   // const copy = template.cloneNode(true);
-  const filterWinter = dataer.filter((data) => data.Arstid1+Arstid2+Arstid3+Arstid4 === 'Vinter');
-console.log(filterWinter)
+  const filterWinter = data.filter((data) => data.Arstid1 + Arstid2 + Arstid3 + Arstid4 === "Vinter");
+  console.log(filterWinter);
 
-
-
-
-copy
-.querySelector(".menu_buttons").href=`productlist.html?id=${data.Arstid1+Arstid2+Arstid3+Arstid4}`;
-
-  }
-  
-
+  copy.querySelector(".menu_buttons").href = `productlist.html?id=${data.Arstid1 + Arstid2 + Arstid3 + Arstid4}`;
+}
 
 // function showData(data){
 //   const template = document.querySelector(".winter-button").content;
@@ -49,13 +42,9 @@ copy
 //   const filterWinter = data.filter((data)=> data.Arstid1+Arstid2+Arstid3+Arstid4 === 'Vinter');
 // console.log(filterWinter)
 
-
 // copy
 // .querySelector(".menu_buttons").href=`productlist.html?id=${data.Arstid1+Arstid2+Arstid3+Arstid4}`;
 // }
-
-
-
 
 // let filterWinter = {Arstid1: 'Vinter', Arstid2: 'Vinter', Arstid3: 'Vinter', Arstid4: 'Vinter'};
 
@@ -76,11 +65,10 @@ copy
 
 // burgermenu
 
-const menu_btn = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.mobile-nav')
+const menu_btn = document.querySelector(".hamburger");
+const mobile_menu = document.querySelector(".mobile-nav");
 
-menu_btn.addEventListener('click', function(){
-    menu_btn.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
-
+menu_btn.addEventListener("click", function () {
+  menu_btn.classList.toggle("is-active");
+  mobile_menu.classList.toggle("is-active");
 });
